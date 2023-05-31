221ba00dc4ee738cdc9f45257e26c4ded3226dda
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2023-05-31 15:19:43 -0300

    feat: added checkbox and radio components

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-checkbox class="w-100" :hint="hint" v-model="model" :label="label" :color="color" :rules="rules" :density="density"
+    :loading="loading" :hide-details="hideDetails" :value="value" />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Checkbox",
+  props: {
+    color: {
+      type: String,
+      default: 'primary'
+    },
+    density: {
+      type: String,
+      default: 'comfortable'
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    hideDetails: {
+      type: Boolean,
+      default: false
+    },
+    modelValue: {
+      default: () => []
+    },
+    value: {
+      default: '',
+    },
+    hint: {
+      type: String,
+      default: ""
+    },
+    rules: {
+      default: () => []
+    },
+  },
+
+  computed: {
+    model: {
+      get() {
+        return this.modelValue;
+      },
+      set(modelValue: String[]) {
+        this.$emit("update:modelValue", modelValue);
+      },
+    },
+  },
+});
+</script>
